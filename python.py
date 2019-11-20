c191a674a4a0e3d4be38d8da3735bfd9fbd6aadc
akjeyaramaji <akjeyaramaji@AK-jeyaram-Aji.local>
2019-11-20 18:30:51 +0530

    Changed to odd or even program

--- python.py
+++ python.py
@@ -1,2 +1,5 @@
-a=input('enter your data')
-print("Your entered data is : ",a);
+a=input('Enter your data for checking odd or even:')
+if(a%2==0):
+	print('Your entered data is even')
+else:
+	print("Your entered data is odd")
